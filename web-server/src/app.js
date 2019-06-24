22b86e40c9f76bf96f59466c316ae66ed26dcd50
Daniel Galván <danygs@gmail.com>
2019-06-24 12:56:40 -0500

    Node.js course Section 7 Lesson 52

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,22 @@
+/* eslint-disable no-console */
 const path = require('path')
-
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 const port = 3000
 
+// Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -27,7 +35,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    helpText: 'This is some helpful text'
+    helpText: 'This is some helpful text',
+    title: 'Help',
+    name: 'Daniel Galván'
   })
 })
 
@@ -35,6 +45,22 @@ app.get('/weather', (req, res) => {
   res.send({
     forecast: 'It is sunny',
     location: 'Mexico City'
+  })
+})
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMessage: 'Help article not found',
+    name: 'Daniel Galván'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMessage: 'Page not found',
+    name: 'Daniel Galván'
   })
 })
 
