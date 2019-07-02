b90e2aaa098dd75333e5de860cc1f70007babfee
Daniel Galván <daniel@triyond.com>
2019-07-02 00:06:11 -0500

    Last commit before production

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,8 +1,6 @@
 const sgMail = require('@sendgrid/mail')
 
-const sendgridAPIKey = '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'
-
-sgMail.setApiKey(sendgridAPIKey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
   sgMail.send({
